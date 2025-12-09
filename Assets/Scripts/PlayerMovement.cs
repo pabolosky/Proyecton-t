1882f08b89f1540c1021f4a317d5b2f6f54c0a67
pabolo <pgarest05@gmail.com>
2025-12-09 20:46:51 +0100

    animtaion + roll

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -6,11 +6,16 @@ public class PlayerMovement : MonoBehaviour
     float horizontalInput;
     float moveSpeed = 5f;
     bool isFacingRight = false;
+    
+    float rollPower = 1f;
+    bool isRolling = false;
+
     float jumpPower = 5f;
     bool isGrounded = false;
 
     Rigidbody2D rb;
     Animator animator;
+    bool isBusy;
 
     // Start is called before the first frame update
     void Start()
@@ -26,24 +31,40 @@ public class PlayerMovement : MonoBehaviour
 
         FlipSprite();
 
+        if (isBusy) return;
+
+        // Maybe quitar el isGrounded para doble salto
         if(Input.GetButtonDown("Jump") && isGrounded)
         {
             rb.velocity = new Vector2(rb.velocity.x, jumpPower);
             isGrounded = false;
             animator.SetBool("isJumping", !isGrounded);
         }
+
+        if (Input.GetKeyDown(KeyCode.LeftShift) && isGrounded)
+        {
+            isRolling = true;
+            animator.SetTrigger("Roll");
+            rb.velocity = new Vector2((isFacingRight ? 1 : -1) * rollPower, rb.velocity.y);
+        }
     }
 
     private void FixedUpdate()
     {
-        rb.velocity = new Vector2(horizontalInput * moveSpeed, rb.velocity.y);
+        if (!isRolling)
+        {
+            rb.velocity = new Vector2(horizontalInput * moveSpeed, rb.velocity.y);
+        }
+
         animator.SetFloat("xVelocity", Math.Abs(rb.velocity.x));
         animator.SetFloat("yVelocity", rb.velocity.y);
     }
 
     void FlipSprite()
     {
-        if(isFacingRight && horizontalInput < 0f || !isFacingRight && horizontalInput > 0f)
+        if (isRolling) return;
+
+        if (isFacingRight && horizontalInput < 0f || !isFacingRight && horizontalInput > 0f)
         {
             isFacingRight = !isFacingRight;
             Vector3 ls = transform.localScale;
@@ -57,4 +78,10 @@ public class PlayerMovement : MonoBehaviour
         isGrounded = true;
         animator.SetBool("isJumping", !isGrounded);
     }
+
+    public void EndRoll()
+    {
+        isRolling = false;
+    }
+
 }
